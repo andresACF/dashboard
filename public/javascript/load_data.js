8c3a2ca5979d2fbd8deab4e0cc376c1c4813cdce
andresACF <107152458+andresACF@users.noreply.github.com>
2023-11-17 20:11:13 -0500

    cambios

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -32,7 +32,71 @@ let cargarPrecipitacion = () => {
     precipitacionMaxValue.textContent = `Max ${max} [mm]`
   
   }
+
+  let cargarUv = () => {
+
+    //Obtenga la función fechaActual
+    let actual = fechaActual();
+    //Defina un arreglo temporal vacío
+    let datos = []
+    //Itere en el arreglo tiempoArr para filtrar los valores de precipitacionArr que sean igual con la fecha actual
+    for (let index = 0; index < tiempoArr.length; index++) {
+        const tiempo = tiempoArr[index];
+        const precipitacion = uvArr[index]
+    
+        if(tiempo.includes(actual)) {
+          datos.push(precipitacion)
+        }
+      }
+    //Con los valores filtrados, obtenga los valores máximo, promedio y mínimo
+    let max = Math.max(...datos)
+    let min = Math.min(...datos)
+    let sum = datos.reduce((a, b) => a + b, 0);
+    let prom = (sum / datos.length) || 0;
+    //Obtenga la referencia a los elementos HTML con id precipitacionMinValue, precipitacionPromValue y precipitacionMaxValue
+    let uvMinValue = document.getElementById("uvMinValue")
+    let uvPromValue = document.getElementById("uvPromValue")
+    let uvMaxValue = document.getElementById("uvMaxValue")
+    //Actualice los elementos HTML con los valores correspondientes
+    uvMinValue.textContent = `Min ${min} [mm]`
+    uvPromValue.textContent = `Prom ${ Math.round(prom * 100) / 100 } [mm]`
+    uvMaxValue.textContent = `Max ${max} [mm]`
   
+  }
+
+  let cargarTemperatura = () => {
+
+    //Obtenga la función fechaActual
+    let actual = fechaActual();
+    //Defina un arreglo temporal vacío
+    let datos = []
+    //Itere en el arreglo tiempoArr para filtrar los valores de precipitacionArr que sean igual con la fecha actual
+    for (let index = 0; index < tiempoArr.length; index++) {
+        const tiempo = tiempoArr[index];
+        const precipitacion = temperaturaArr[index]
+    
+        if(tiempo.includes(actual)) {
+          datos.push(precipitacion)
+        }
+      }
+    //Con los valores filtrados, obtenga los valores máximo, promedio y mínimo
+    let max = Math.max(...datos)
+    let min = Math.min(...datos)
+    let sum = datos.reduce((a, b) => a + b, 0);
+    let prom = (sum / datos.length) || 0;
+    //Obtenga la referencia a los elementos HTML con id precipitacionMinValue, precipitacionPromValue y precipitacionMaxValue
+    let temperaturaMinValue = document.getElementById("temperaturaMinValue")
+    let temperaturaPromValue = document.getElementById("temperaturaPromValue")
+    let temperaturaMaxValue = document.getElementById("temperaturaMaxValue")
+    //Actualice los elementos HTML con los valores correspondientes
+    temperaturaMinValue.textContent = `Min ${min} [mm]`
+    temperaturaPromValue.textContent = `Prom ${ Math.round(prom * 100) / 100 } [mm]`
+    temperaturaMaxValue.textContent = `Max ${max} [mm]`
+  
+  }
+
+  cargarTemperatura();
+  cargarUv();
   cargarPrecipitacion();
 
   let cargarFechaActual = () => {
@@ -45,7 +109,7 @@ let cargarPrecipitacion = () => {
     tituloH6.textContent = fechaActual()
   }
   
-
+ //guia 9
   
   cargarFechaActual()
 
@@ -97,6 +161,93 @@ let cargarPrecipitacion = () => {
   }
 
   cargarOpenMeteo()
+
+  let cargarOpenMeteo2 = () => {
+
+    //URL que responde con la respuesta a cargar
+    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=rain&timezone=auto&forecast_days=14'; 
   
+    fetch( URL )
+      .then(responseText => responseText.json())
+      .then(responseJSON => {
+        
+          //Respuesta en formato JSON
+      
+          //Referencia al elemento con el identificador plot
+          let plotRef = document.getElementById('plot2');
+      
+          let labels = responseJSON.hourly.time;
+
+        // Datos de lluvia para el gráfico de barras
+        let rainData = responseJSON.hourly.rain;
+
+        // Objeto de configuración del gráfico
+        const data = {
+          labels: labels,
+          datasets: [{
+            label: 'Rain Data',
+            data: rainData,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            borderWidth: 1
+          }]
+        };
+
+        const config = {
+          type: 'bar',
+          data: data,
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          },
+        };
+
+
+        //Objeto con la instanciación del gráfico
+        let chart1 = new Chart(plotRef, config);
+
+
+        console.log(responseJSON);
+
+        //Respuesta en formato JSON
+
+      })
+      .catch(console.error);
   
-  
+  }
+
+  cargarOpenMeteo2()
+
+
+  // guia 10
+
+
+
+
+
+  let loadExternalTable = async() => {
+    console.log("Gestion de riesgo")
+
+     let proxyURL = 'https://cors-anywhere.herokuapp.com/'
+     let url = proxyURL + 'https://www.gestionderiesgos.gob.ec/monitoreo-de-inundaciones/'
+
+
+    let response= await fetch(url)
+    let responseText =await response.text()
+
+    const parser= await new DOMParser();
+    const xml =await parser.parseFromString(responseText, "text/html")
+
+    let table= await xml.querySelector("#postcontent table")
+    document.getElementById("monitoreo").innerHTML=table.outerHTML
+
+
+
+
+   }
+
+ 
+   loadExternalTable()
